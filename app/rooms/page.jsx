5a874214017b74c896d5bc0ef22448de94d6a869
김태윤 <xodbs1758@gmail.com>
2026-03-15 20:07:20 +0900

    [Feature]: Room 페이지 메인문구 애니메이션 추가 페이지 마운트 시 메인문구 Fade up 애니메이션 추가 Resolved

--- app/rooms/page.jsx
+++ app/rooms/page.jsx
@@ -1,21 +1,54 @@
+"use client";
 import { data } from "@/dummy/data";
 import Room from "@/Room";
+import { motion, easeOut } from "framer-motion";
 
 const Page = () => {
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20, filter: "blur(5px)" },
+    visible: (i) => ({
+      opacity: 1,
+      y: 0,
+      filter: "blur(0px)",
+      transition: { delay: i * 0.2, duration: 0.5, ease: easeOut },
+    }),
+  };
   return (
     <section className="bg-[#00083046] py-20">
       <div className="container mx-auto">
-        <div className="pb-10">
-          <h2 className="text-5xl text-white text-center mb-4">
+        <motion.div
+          className="pb-10"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.h2
+            className="text-5xl text-white text-center mb-4"
+            variants={itemVariants}
+            custom={0}
+          >
             당신의 코딩력, 컴퓨팅사고력, 디버깅 능력은?
-          </h2>
-          <p className="text-center text-white">
+          </motion.h2>
+          <motion.p
+            className="text-center text-white"
+            variants={itemVariants}
+            custom={1}
+          >
             IT_ESCAPE는 참가자가 실제 상황을 방불케 하는 특별한 테마 룸에 갇힌
             채 60분 이내에
             <br />
             주어진 미션을 해결하여 탈출해야하는 신개념 문화/여가 시설입니다.
-          </p>
-        </div>
+          </motion.p>
+        </motion.div>
         {/* 3열 그리드로 표현하는 방탈출 목록 */}
         <div className="grid grid-cols-3 gap-10 justify-center items-center">
           {data.map((v, i) => (
